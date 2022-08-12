74f98396c70e2e380bd77e7ec8a707332d03fe57
luxxxfero <aleks.ferolux@gmail.com>
2022-08-12 11:11:47 +0500

    add redirects

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/sonnik/word/:path*',
+        destination: '/толкование/:path*',
+        permanent: true
+      },
+      {
+        source: '/sonnik/letter/:path*',
+        destination: '/буква/:path*',
+        permanent: true
+      }
+    ]
+  },
   async rewrites() {
     return [
       {
