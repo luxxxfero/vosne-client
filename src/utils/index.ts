029f5f7093897b5993108e58c079273ff9a569bd
luxxxfero <aleks.ferolux@gmail.com>
2022-08-21 16:40:56 +0500

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,29 +5,23 @@ export const getKeys = (word: string, length: number): string[] => {
   const arr = [];
   const letter = getLetter(word);
   for (let i = 0; i < length; i++) {
-    if (keys[i % 6] === "Толкование сна") {
+    if (keys[i % 5] === "Толкование сна") {
       arr.push(`Толкование сна ${word}`)
-    } else if (keys[i % 6] === "Значение сна") {
+    } else if (keys[i % 5] === "Значение сна") {
       arr.push(`Значение сна ${word}`)
-    } else if (keys[i % 6] === "во сне") {
+    } else if (keys[i % 5] === "во сне") {
       arr.push(`${word} во сне`)
-    } else if (keys[i % 6] === "Снится") {
+    } else if (keys[i % 5] === "Снится") {
       if (letter === "я") {
         arr.push(`Снятся ${word}`)
       } else {
         arr.push(`Снится ${word}`)
       }
-    } else if (keys[i % 6] === "К чему снится") {
+    } else if (keys[i % 5] === "К чему снится") {
       if (letter === "я") {
         arr.push(`К чему снятся ${word}`)
       } else {
         arr.push(`К чему снится ${word}`)
-      }
-    } else if (keys[i % 6] === "Приснилась") {
-      if (letter === "я") {
-        arr.push(`Приснились ${word}`)
-      } else {
-        arr.push(`Приснилась ${word}`)
       }
     }
   }
